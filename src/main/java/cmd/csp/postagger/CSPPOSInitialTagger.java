232a4563c921c300724bbcefd4a7fffc5fcab9d3
swelcker <welckerstefan@gmail.com>
2019-10-21 01:18:39 +0200

    notes

--- src/main/java/cmd/csp/postagger/CSPPOSInitialTagger.java
+++ src/main/java/cmd/csp/postagger/CSPPOSInitialTagger.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 /**
- * @author DatQuocNguyen
+ * @author DatQuocNguyen, Stefan Welcker modifications
  * 
  */
 public class CSPPOSInitialTagger
--- src/main/java/cmd/csp/postagger/Utils.java
+++ src/main/java/cmd/csp/postagger/Utils.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- * @author DatQuocNguyen
+ * @author DatQuocNguyen, Stefan Welcker modifications
  * 
  */
 public class Utils
